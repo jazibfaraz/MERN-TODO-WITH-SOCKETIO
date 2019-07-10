6d7e3a6aa03a6903edd00b4d4d1b68c93c4e4f26
jazibfaraz <40723344+jazibfaraz@users.noreply.github.com>
2019-07-10 18:22:34 +0500

    Real-Time Todo using Socket.io

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends React.Component {
 
     //eastablishing a connection on socket io
     // socket = io('http://localhost:4000');
-    socket = io(window.location.hostname + ':' + 4000)
+    socket = io(window.location.hostname)
     socket.on('connect', () => console.log('connected')
     )
 
